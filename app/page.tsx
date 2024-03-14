8a37ac6902127c5f74b8756f46253109822006b5
NISHANT SURENDRA GHARPURE <gharpurenishant@gmail.com>
2024-03-14 12:48:26 +0530

    misc

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import DoubleSpace from '@/components/common/DoubleSpace';
 
 const UnityComponent = () => {
   return (
-    <div className='h-screen flex items-center justify-center bg-cover bg-center relative' >
+    <div className='h-screen flex items-center justify-center bg-cover bg-center relative' >  
       <Image className='w-full h-full object-cover' src='/backgoundImage.avif' alt='' height={1000} width={1000} />
       <div className='absolute w-full top-0 flex'>
         <Image className='mt-4 ml-6' src={Logo} alt='' height={150} width={150} />
